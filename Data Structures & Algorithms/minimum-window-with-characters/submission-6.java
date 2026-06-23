705e12aecdf9ed543cb0e1d84f43dd8daa516489
Yeshwanth B E <132122039+YeshwanthBE@users.noreply.github.com>
2026-06-23 22:46:03 +0530

    Add: minimum-window-with-characters - submission-6

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-6.java
@@ -0,0 +1,62 @@
+class Solution {
+
+    public int getAlphaASCII(char ch){
+        return ch > 'Z' ? ch -'a' + 26 : ch - 'A';
+    }
+
+    public int[] getCharFreq(String s){
+        int[] charFreq = new int[52];
+        
+        for(var ch : s.toCharArray()){
+            ++charFreq[getAlphaASCII(ch)];
+        }
+        
+        return charFreq;
+    }
+
+    public String minWindow(String s, String t) {
+
+        if(s.length() < t.length()){
+            return "";
+        }
+
+        int[] charFreq2 = getCharFreq(t);
+
+        int uniqueChars = 0;
+        for(var i : charFreq2){
+            if(i!=0){
+                uniqueChars++;
+            }
+        }
+
+        int[] charFreq1 = new int[52]; int matches = 0;
+
+        int resultStartIndex = 0,resultEndIndex = 1001;
+
+        for(int i=0,j=0; j<s.length();){
+            int charASCII = getAlphaASCII(s.charAt(j));
+            ++charFreq1[charASCII];
+            if(charFreq1[charASCII] == charFreq2[charASCII]){
+                    matches++;
+            }
+
+            while(matches == uniqueChars){
+                if(resultEndIndex-resultStartIndex > j- i){
+                    resultStartIndex = i;
+                    resultEndIndex = j;
+                }
+                charASCII = getAlphaASCII(s.charAt(i));
+                if(charFreq1[charASCII] == charFreq2[charASCII]){
+                    matches--;
+                }
+                --charFreq1[charASCII];
+                i++;
+                
+            }
+            j++;
+        }
+
+
+        return resultEndIndex == 1001 ? "" : s.substring(resultStartIndex,resultEndIndex+1);
+    }
+}
