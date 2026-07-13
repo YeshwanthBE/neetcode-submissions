c6d2a0ef2357921ddaf888b6cf9a1320f8ee6806
Yeshwanth B E <132122039+YeshwanthBE@users.noreply.github.com>
2026-07-13 14:01:45 +0530

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    public int maxDepth(TreeNode root) {
+
+        if(root == null){
+            return 0;
+        }
+
+
+        Deque<TreeNode> nodeStack = new ArrayDeque<>();
+        Deque<Integer> depthStack = new ArrayDeque<>();
+        nodeStack.push(root);depthStack.push(1);
+
+        int maxDepth = -1;
+        while(!nodeStack.isEmpty()){
+            TreeNode node = nodeStack.pop();
+            int depth = depthStack.pop();
+            
+            maxDepth = Math.max(maxDepth,depth);
+            if(node.left != null){
+                nodeStack.push(node.left);
+                depthStack.push(depth+1);
+            }
+
+            if(node.right != null){
+                nodeStack.push(node.right);
+                depthStack.push(depth+1);
+            }
+        }
+
+        return maxDepth;
+    }
+}
