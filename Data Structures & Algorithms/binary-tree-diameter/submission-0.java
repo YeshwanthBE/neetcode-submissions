ea41b70c5bcd876c091fa777b2e7b561b2aa68af
Yeshwanth B E <132122039+YeshwanthBE@users.noreply.github.com>
2026-07-13 19:14:33 +0530

    Add: binary-tree-diameter - submission-0

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-0.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    public Deque<TreeNode> getPostOrderStack(TreeNode root){
+        
+        Deque<TreeNode> nodeStack = new ArrayDeque<>();
+        Deque<TreeNode> nodeStack2 = new ArrayDeque<>();
+
+        nodeStack.push(root);
+
+        while(!nodeStack.isEmpty()){
+            TreeNode node = nodeStack.pop();
+            nodeStack2.push(node);
+
+            if(node.left != null){
+                nodeStack.push(node.left);
+            }
+
+            if(node.right != null){
+                nodeStack.push(node.right);
+            }
+        }
+
+        return nodeStack2;
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        Deque<TreeNode> nodeStack = getPostOrderStack(root);
+        Map<TreeNode,Integer> nodeVsDepth = new HashMap();
+        int diameter = 0;
+        while(!nodeStack.isEmpty()){
+            TreeNode node = nodeStack.pop();
+            int leftDepth = nodeVsDepth.getOrDefault(node.left,0);
+            int rightDepth = nodeVsDepth.getOrDefault(node.right,0);
+            
+            nodeVsDepth.put(node,1+Math.max(leftDepth,rightDepth));
+
+            diameter = Math.max(diameter,leftDepth+rightDepth);
+        }
+        return diameter;
+    }
+}
