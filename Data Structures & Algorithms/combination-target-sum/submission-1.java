412ba9e3e24908eda7a0f12055abd2b3becfebd9
Yeshwanth B E <132122039+YeshwanthBE@users.noreply.github.com>
2026-07-30 11:07:56 +0530

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    public void targetSum(int[] nums, int target, int curSum,int index, List<Integer> resultList){
+        
+        if(curSum == target){
+            result.add(new ArrayList<>(resultList));
+            return;
+        }
+        
+        if(index==nums.length || curSum >target){
+            return ;
+        }
+
+        
+
+        resultList.add(nums[index]);
+        targetSum(nums,target,curSum+nums[index],index,resultList);
+        resultList.remove(resultList.size()-1);
+        targetSum(nums,target,curSum,index+1,resultList);
+    
+    }
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        targetSum(nums,target,0,0,new ArrayList<>());
+        return result;
+    }
+}
